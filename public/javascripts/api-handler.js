6991046280f33865b915aaa1642338e52e24363e
Paricia Muñoz <patrimdm96@gmail.com>
2020-11-23 18:19:04 +0000

    API WORKS IN DOM

--- public/javascripts/api-handler.js
+++ public/javascripts/api-handler.js
@@ -11,11 +11,7 @@ class APIhandler {
         })
     }
 
-    // const apiKey = process.env.APIkey
-
-    getTopRatedMovies() {
-        this.axiosApp.get(`/movie/top_rated?api_key=95ad659b54a1464fdb415db2270f7402`)
-    }
+    getTopRatedMovies = () => this.axiosApp.get(`/movie/top_rated?api_key=95ad659b54a1464fdb415db2270f7402`)
 
 }
 
