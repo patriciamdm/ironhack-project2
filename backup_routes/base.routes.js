e12f294cc311ecca7cd582a910250b3e14655b65
jorboare <jordi.b.arevalo@gmail.com>
2020-11-26 19:56:55 +0100

    Glide local and partials config

--- backup_routes/base.routes.js
+++ backup_routes/base.routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const Movie = require('../models/movie.model')
+const Series = require('../models/series.model.js')
 
 const isLogged = (req) => req.isAuthenticated() === true
 const isNotLogged = (req) => req.isAuthenticated() === false
@@ -9,8 +11,45 @@ const isNotLogged = (req) => req.isAuthenticated() === false
 
 // ENDPOINTS
 
-router.get('/', (req, res) => res.render('index', { isLogged: isLogged(req), isNotLogged: isNotLogged(req) }))
 
 
+router.get('/', (req, res, next) => {
+    let popularMovies
+
+    Movie
+        .find()
+        .sort({ popularity: -1 })
+        .then(allMovies => popularMovies = allMovies)
+        .then(() => Series.find().sort({ popularity: -1 }))
+        .then(allSeries => {
+            res.render('index', { popularMovies: popularMovies, popularSeries: allSeries, isLogged: isLogged(req), isNotLogged: isNotLogged(req) })
+        }).catch(err => next(new Error(err)))
+
+    // apiHandler
+    //     .get(`/movie/popular?api_key=95ad659b54a1464fdb415db2270f7402`)
+    //     .then(allMovies => popularMovies = allMovies.data.results)
+    //     .then(() => apiHandler.get(`/tv/popular?api_key=95ad659b54a1464fdb415db2270f7402`))
+    //     .then(popularSeries => {
+
+    //         const topPopularMovies = popularMovies.splice(0, 10)
+    //         const topPopularSeries = popularSeries.data.results.splice(0, 10)
+    //         res.render('index', { popularMovies: topPopularMovies, popularSeries: topPopularSeries, isLogged: isLogged(req), isNotLogged: isNotLogged(req) })
+    //     })
+    //     .catch(err => next(new Error(err)))
+
+
+    // Series
+    //     .find()
+    //     .sort({ popularity: -1 })
+    //     .then(allSeries => {
+    //         res.render('data/series', { allSeries, isLogged: isLogged(req), isNotLogged: isNotLogged(req) })
+    //     })
+    //     .catch(err => next(new Error(err)))
+    // Movie
+    //     .find()
+    //     .sort({ popularity: -1 })
+    //     .then(allMovies => res.render('data/movies', { allMovies, isLogged: isLogged(req), isNotLogged: isNotLogged(req) }))
+    //     .catch(err => next(new Error(err)))
+})
 module.exports = router
 
